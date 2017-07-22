9fe616368fc0a2138b45ac4b1975aef3d617a71e
Tim Lehmann <l_@freenet.de>
2017-07-22 10:30:45 +0200

    Ready to code

---
+++ src/test/java/de/lehmann/lehmannorm/test/InitUnitTest.java
@@ -0,0 +1,16 @@
+package de.lehmann.lehmannorm.test;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author Tim Lehmann
+ */
+public class InitUnitTest {
+
+	@Test
+	public void helloWorld() {
+		
+		System.out.println("LehmannORM");
+	}
+}
