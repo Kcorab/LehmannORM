45325530d0741b39cbbf101702fd2bb74bc580aa
Tim Lehmann <l_@freenet.de>
2018-01-27 22:23:56 +0100

    extend of dao class by caching method
    - cache method added
    - unit test for dao class updated

--- src/main/java/de/lehmann/lehmannorm/logic/Dao.java
+++ src/main/java/de/lehmann/lehmannorm/logic/Dao.java
@@ -3,7 +3,12 @@ package de.lehmann.lehmannorm.logic;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
+
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
 
 import de.lehmann.lehmannorm.entity.AbstractEntity;
 import de.lehmann.lehmannorm.entity.structure.EntityColumnInfo;
@@ -21,8 +26,11 @@ import de.lehmann.lehmannorm.logic.sqlbuilder.IStatementBuilder.DefaultBuilderBu
  */
 public class Dao<E extends AbstractEntity<PK>, PK> {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Dao.class);
+
+    private static final Map<Connection, Map<Class<AbstractEntity<?>>, Dao<?, ?>>> conndaoCache = new HashMap<>();
+
     /**
-     *
      * @param connection
      * @param entityType
      * @return
@@ -37,24 +45,59 @@ public class Dao<E extends AbstractEntity<PK>, PK> {
     }
 
     /**
-     *
      * @param connection
-     * @param entityType
+     * @param entity
      * @return
      * @throws SQLException
      * @throws InstantiationException
      * @throws IllegalAccessException
      */
-    private static <E extends AbstractEntity<PK>, PK> Dao<E, PK> createInstance(
-            final Connection connection, final E entityType)
-            throws SQLException, InstantiationException, IllegalAccessException {
-        return new Dao<>(connection, entityType);
+    public static <E extends AbstractEntity<PK>, PK> Dao<E, PK> createInstance(
+            final Connection connection, final E entity)
+            throws SQLException {
+        return new Dao<>(connection, entity);
     }
 
+    /**
+     * @param entity
+     * @return never null
+     * @throws SQLException
+     * @throws IllegalAccessException
+     * @throws InstantiationException
+     */
+    @SuppressWarnings("unchecked")
+    public static <E extends AbstractEntity<PK>, PK> Dao<E, PK> getOrCreateCachedDao(final Connection connection,
+            final Class<E> entityType) throws SQLException, InstantiationException, IllegalAccessException {
+
+        final Map<Class<AbstractEntity<?>>, Dao<?, ?>> daoForEntityMap;
+        final Dao<E, PK> dao;
+
+        if (conndaoCache.containsKey(connection)) {
+            daoForEntityMap = conndaoCache.get(connection);
+
+            if (daoForEntityMap.containsKey(entityType))
+                dao = (Dao<E, PK>) daoForEntityMap.get(entityType);
+            else
+                dao = Dao.createInstance(connection, entityType.newInstance());
+        } else {
+            daoForEntityMap = new HashMap<>();
+
+            dao = Dao.createInstance(connection, entityType.newInstance());
+            daoForEntityMap.put((Class<AbstractEntity<?>>) entityType, dao);
+
+            conndaoCache.put(connection, daoForEntityMap);
+        }
+
+        return dao;
+    }
+
+    private final Connection        connection;
     private final PreparedStatement insertStatement;
     private final PreparedStatement selectStatement;
 
     private Dao(final Connection connection, final E entity) throws SQLException {
+
+        this.connection = connection;
 
         final Set<EntityColumnInfo<Object>> entityColumns = entity.getAllColumns().keySet();
 
--- src/test/java/de/lehmann/lehmannorm/logic/DaoUnitTest.java
+++ src/test/java/de/lehmann/lehmannorm/logic/DaoUnitTest.java
@@ -3,45 +3,94 @@ package de.lehmann.lehmannorm.logic;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import de.lehmann.lehmannorm.AConnectionUnitTest;
-import de.lehmann.lehmannorm.models.PersonTestEntity;
-import de.lehmann.lehmannorm.models.ReligionTestEntity;
-import de.lehmann.lehmannorm.models.TierTestEntity;
+import de.lehmann.lehmannorm.entity.AbstractEntity;
+import de.lehmann.lehmannorm.entity.structure.EntityColumnInfo;
+import de.lehmann.lehmannorm.entity.structure.EntityColumnInfo.ForeignKeyHolder;
+import de.lehmann.lehmannorm.stubs.ConnectionStub;
 
 public class DaoUnitTest extends AConnectionUnitTest {
 
-    private TierTestEntity               entity;
-    private Dao<TierTestEntity, Integer> unitToTest;
+    private Dao<AbstractEntity<Integer>, Integer> unitToTest;
 
-    @Disabled
+    // TEST STATIC METHODS
+
     @Test
-    public void testDaoCaching() throws InstantiationException, IllegalAccessException, SQLException {
+    public void getOrCreateCachedDao() throws InstantiationException, IllegalAccessException, SQLException {
 
-        final TierTestEntity tierA = createNestedEntities();
-        final TierTestEntity tierB = createNestedEntities();
-        unitToTest = Dao.createInstance(connection, TierTestEntity.class);
+        Connection connection;
 
-    }
+        connection = new ConnectionStub();
+        final TestEntityA testEntityA = new TestEntityA();
 
-    private TierTestEntity createNestedEntities() {
+        Assertions.assertEquals(
+                Dao.getOrCreateCachedDao(connection, testEntityA.getClass()),
+                Dao.getOrCreateCachedDao(connection, testEntityA.getClass()));
 
-        final ReligionTestEntity religion = new ReligionTestEntity();
+        connection = new ConnectionStub();
+        final TestEntityB testEntityB = new TestEntityB();
 
-        final PersonTestEntity person = new PersonTestEntity();
-        person.setColumnValue(PersonTestEntity.RELIGION, religion);
-
-        final TierTestEntity tier = new TierTestEntity();
-        tier.setColumnValue(TierTestEntity.BESITZER, person);
-
-        return tier;
+        Assertions.assertEquals(
+                Dao.getOrCreateCachedDao(connection, testEntityB.getClass()),
+                Dao.getOrCreateCachedDao(connection, testEntityB.getClass()));
     }
 
     @Override
     protected Connection createConnection() throws SQLException {
 
         return null;
+    }
+
+    // # MOCKS
+
+    // ## DATA MOCKS
+
+    private static class TestEntityA extends AbstractEntity<Integer> {
+
+        public final static EntityColumnInfo<Integer>     ID   = new EntityColumnInfo<>("ID", Integer.class);
+        public final static EntityColumnInfo<TestEntityB> ID_B =
+                new EntityColumnInfo<>("ID_B", TestEntityB.class, ForeignKeyHolder.THIS_ENTITY_TYPE);
+
+        protected TestEntityA() {
+            super(ID, ID_B);
+        }
+
+        @Override
+        public String getTableName() {
+            return null;
+        }
+    }
+
+    private static class TestEntityB extends AbstractEntity<Integer> {
+
+        public final static EntityColumnInfo<Integer>     ID   = new EntityColumnInfo<>("ID", Integer.class);
+        public final static EntityColumnInfo<TestEntityC> ID_C =
+                new EntityColumnInfo<>("ID_C", TestEntityC.class, ForeignKeyHolder.REFERENCED_ENTITY_TYPE);
+
+        protected TestEntityB() {
+            super(ID, ID_C);
+        }
+
+        @Override
+        public String getTableName() {
+            return null;
+        }
+    }
+
+    private static class TestEntityC extends AbstractEntity<Integer> {
+
+        public final static EntityColumnInfo<Integer> ID = new EntityColumnInfo<>("ID", Integer.class);
+
+        protected TestEntityC() {
+            super(ID);
+        }
+
+        @Override
+        public String getTableName() {
+            return null;
+        }
     }
 }
